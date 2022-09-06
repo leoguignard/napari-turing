15187ef4ae698a9fb7cbaa5adc3a47f35666e193
Léo Guignard <leo.guignard@univ-amu.fr>
2022-09-06 10:16:32 +0200

    small modif in __init__

--- src/napari_turing/__init__.py
+++ src/napari_turing/__init__.py
@@ -2,5 +2,3 @@ __version__ = "0.3.1"
 from ._widget import TuringViewer
 from .Models._TuringPattern import TuringPattern
 from .Models._model_list import *
-
-# __all__ = ("TuringViewer", "TuringPattern", "GrayScott", "Brusselator", "FitzHughNagumo")
